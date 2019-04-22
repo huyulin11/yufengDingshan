4fb8b51038f27d7be57daa636fcde90c1cefd7fd
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-04-22 09:20:15 +0800

    通讯模块地址标注字段改造

--- ProjectBussYufengDingshan/udf/com/kaifantech/component/service/agv/simulator/YufengAgvServerWorker.java
+++ ProjectBussYufengDingshan/udf/com/kaifantech/component/service/agv/simulator/YufengAgvServerWorker.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.kaifantech.bean.iot.client.IotClientBean;
 import com.kaifantech.component.comm.worker.server.IServerWorker;
 import com.kaifantech.util.socket.IConnect;
 import com.kaifantech.util.socket.netty.server.yufeng.YufengAgvNettyServer;
@@ -19,7 +20,7 @@ public class YufengAgvServerWorker implements IServerWorker {
 
 	public synchronized void init() {
 		if (getMap() == null || getMap().size() == 0) {
-			YufengAgvNettyServer simulator = YufengAgvNettyServer.create("" + 8080);
+			YufengAgvNettyServer simulator = YufengAgvNettyServer.create(new IotClientBean(null, "" + 8080));
 			getMap().put(1, simulator);
 			try {
 				simulator.init();
